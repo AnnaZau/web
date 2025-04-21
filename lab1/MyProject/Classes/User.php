040aaee2a702438a7687b850dbfef6b70a2bbc4f
AnnaZau <126783930+AnnaZau@users.noreply.github.com>
2025-04-21 14:44:41 +0300

    Update User.php

--- lab1/MyProject/Classes/User.php
+++ lab1/MyProject/Classes/User.php
@@ -1,24 +1,31 @@
 <?php
-// User.php
+declare(strict_types=1);
+
 namespace MyProject\Classes;
 
-class User {
-    // Публичные свойства
-    public $name;
-    public $login;
-    
-    // Приватное свойство
-    private $password;
+class User extends AbstractUser {
+    private static int $userCount = 0; // Статическое свойство для подсчета экземпляров
+    protected string $login; // Добавлено свойство login
 
-    // Конструктор для инициализации свойств
-    public function __construct($name, $login, $password) {
-        $this->name = $name;
-        $this->login = $login;
-        $this->password = $password;
+    //Конструктор класса User
+    public function __construct(string $username, string $login, string $password) {
+        parent::__construct($username, $password); // Передаем username и password в родительский класс
+        $this->login = $login; // Инициализация свойства login
+        self::$userCount++; // Инкремент счетчика при создании нового экземпляра
+    }
+    
+    public static function getUserCount(): int {
+        return self::$userCount; // Метод для получения количества экземпляров
     }
 
-    // Метод для вывода информации о пользователе
+    //Метод для отображения информации о пользователе
     public function showInfo() {
-        echo "Имя: {$this->name}, Логин: {$this->login}<br>";
+        echo "Username: {$this->username}, Login: {$this->login}\n";
+    }
+
+    //Деструктор класса User
+
+    public function __destruct() {
+        echo "Пользователь {$this->login} удален.\n";
     }
 }
