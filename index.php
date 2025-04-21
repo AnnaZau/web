ec9796447086c3573d803bf63a10c65a3c7ec463
AnnaZau <126783930+AnnaZau@users.noreply.github.com>
2025-04-21 12:33:17 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Проектирование архитектуры корпоративных информационных систем</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+
+<nav>
+        <lu><a> Лабораторная работа №1 </a></lu>
+        <li><a href="http://f1097116.xsph.ru/lab1/MyProject/users.php">Задание 1</a></li>
+</nav>
+
+</body>
+</html>
