5048c84bae3c7acf2a933504691741e1a31fd1c6
AnnaZau <126783930+AnnaZau@users.noreply.github.com>
2025-04-26 10:21:28 +0300

    Update SuperUserInterface.php

--- lab1/MyProject/Classes/SuperUserInterface.php
+++ lab1/MyProject/Classes/SuperUserInterface.php
@@ -4,5 +4,5 @@ declare(strict_types=1);
 namespace MyProject\Classes;
 
 interface SuperUserInterface {
-    public function getInfo(): array; // Объявляем метод getInfo()
+    public function getInfo(); // Объявляем метод getInfo()
 }
