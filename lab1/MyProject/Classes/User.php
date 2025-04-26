5a02f8b25d91158c5670464749f58f11b359ea4e
AnnaZau <126783930+AnnaZau@users.noreply.github.com>
2025-04-26 10:21:49 +0300

    Update User.php

--- lab1/MyProject/Classes/User.php
+++ lab1/MyProject/Classes/User.php
@@ -1,31 +1,27 @@
 <?php
 declare(strict_types=1);
-
 namespace MyProject\Classes;
 
-class User extends AbstractUser {
-    private static int $userCount = 0; // Статическое свойство для подсчета экземпляров
-    protected string $login; // Добавлено свойство login
+class User extends AbstractUser{ 
 
-    //Конструктор класса User
-    public function __construct(string $username, string $login, string $password) {
-        parent::__construct($username, $password); // Передаем username и password в родительский класс
-        $this->login = $login; // Инициализация свойства login
-        self::$userCount++; // Инкремент счетчика при создании нового экземпляра
-    }
+    public $name; 
+    public $login; 
+    public $password;
+    public static $userCount = 0;
     
-    public static function getUserCount(): int {
-        return self::$userCount; // Метод для получения количества экземпляров
+    function showInfo(){
+         echo "<p><b>name:</b> {$this->name} <b>login:</b> {$this->login} <b>password:</b> {$this->password}</p>";
+        
+     }
+
+    function __construct($name, $login, $password){  //Конструктор
+      $this->name = $name;
+      $this->login = $login;
+      $this->password = $password;
+      self::$userCount++;
     }
-
-    //Метод для отображения информации о пользователе
-    public function showInfo() {
-        echo "Username: {$this->username}, Login: {$this->login}\n";
-    }
-
-    //Деструктор класса User
-
-    public function __destruct() {
-        echo "Пользователь {$this->login} удален.\n";
+  
+    function __destruct(){  // Деструктор
+      echo "<p>Пользователь <b>{$this->login}</b> удален </p>";
     }
 }
