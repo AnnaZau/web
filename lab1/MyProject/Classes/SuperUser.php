5004b62456c2d6908086c0018ad5aaf0342ae40b
AnnaZau <126783930+AnnaZau@users.noreply.github.com>
2025-04-26 10:21:02 +0300

    Update SuperUser.php

--- lab1/MyProject/Classes/SuperUser.php
+++ lab1/MyProject/Classes/SuperUser.php
@@ -1,35 +1,29 @@
 <?php
 declare(strict_types=1);
-
 namespace MyProject\Classes;
 
-require_once 'User.php'; // Подключение класса User
-require_once 'SuperUserInterface.php'; // Подключение интерфейса SuperUserInterface
+class SuperUser extends User implements SuperUserInterface{
+   public $role;
+   public static $superUserCount = 0;
+   
+   function __construct($name, $login, $password, $role) {
+      $this->name = $name;
+      $this->login = $login;
+      $this->password = $password;
+      $this->role = $role;
+      self::$superUserCount++;
+   }
+   
+   function showInfo(){
+        echo "<p><b>name:</b> {$this->name} <b>login:</b> {$this->login} <b>password:</b> {$this->password} <b>role:</b> {$this->role} </p>";
+   }  
+	
+   function getInfo()
+   {
+       return print_r(['name' => $this->name,
+               'login' => $this->login,
+               'password' => $this->password,
+               'role' => $this->role ]);
 
-class SuperUser extends User implements SuperUserInterface {
-    private static int $superUserCount = 0; // Статическое свойство для подсчета экземпляров
-    public string $role;
-
-    public function __construct(string $name, string $login, string $password, string $role) {
-        parent::__construct($name, $login, $password); // Вызов родительского конструктора
-        $this->role = $role; // Установка значения для свойства role
-        self::$superUserCount++; // Инкремент счетчика при создании нового экземпляра
-    }
-    
-    public static function getSuperUserCount(): int {
-        return self::$superUserCount; // Метод для получения количества экземпляров
-    }
-
-    public function showInfo(): void {
-        parent::showInfo(); // Вызов метода showInfo() родительского класса
-        echo "Роль: {$this->role}\n"; // Вывод информации о роли
-    }
-    
-    public function getInfo(): array {
-        return [
-            'имя пользователя' => $this->name, // Используем унаследованное свойство $name
-            'логин' => $this->login, // Используем унаследованное свойство $login
-            'роль' => $this->role // Собственное свойство role
-        ];
-    }
+   }
 }
