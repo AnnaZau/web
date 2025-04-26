12068ae7dfde2b7ed34ef8376cd2dd02479883ee
AnnaZau <126783930+AnnaZau@users.noreply.github.com>
2025-04-26 10:34:41 +0300

    Update index.php

--- index.php
+++ index.php
@@ -8,8 +8,31 @@
 <body>
 
 <nav>
-        <lu><a> Лабораторная работа №1 </a></lu>
+        <lu><a> Лабораторная работа № 1</a></lu>
         <li><a href="http://f1097116.xsph.ru/lab1/MyProject/users.php">Задание 1</a></li>
+        <li><a href="http://f1097116.xsph.ru/lab1/MyProject/uml.html">Диаграмма</a></li>
+        <lu><a> Лабораторная работа № 2</a></lu>
+        <li> <a href="http://f1097116.xsph.ru/lab2/FactoryMethod.php">Factory Method</a></li>
+        <li><a href="http://f1097116.xsph.ru/lab2/AbstractFactory.php">Abstract Factory</a></li>
+        <li><a href="http://f1097116.xsph.ru/lab2/Builder.php">Builder</a></li>
+        <li><a href="http://f1097116.xsph.ru/lab2/Singleton.php">Singleton</a></li>
+        <li><a href="http://f1097116.xsph.ru/lab2/Adapter.php">Adapter</a></li>
+        <li><a href="http://f1097116.xsph.ru/lab2/Bridge.php">Bridge</a></li>
+        <li><a href="http://f1097116.xsph.ru/lab2/Composite.php">Composite</a></li>
+        <li><a href="http://f1097116.xsph.ru/lab2/Decorate.php">Decorator</a></li>
+        <li><a href="http://f1097116.xsph.ru/lab2/Facade.php">Facade</a></li>
+        <li><a href="http://f1097116.xsph.ru/lab2/Flyweight.php">Flyweight</a></li>
+        <li><a href="http://f1097116.xsph.ru/lab2/Proxy.php">Proxy</a></li>
+        <li><a href="http://f1097116.xsph.ru/lab2/ChainOfResponsibility.php">Chain of Responsibility</a></li>
+        <li><a href="http://f1097116.xsph.ru/lab2/Command.php">Command</a></li>
+        <li><a href="http://f1097116.xsph.ru/lab2/Iterator.php">Iterator</a></li>
+        <li><a href="http://f1097116.xsph.ru/lab2/Mediator.php">Mediator</a></li>
+        <li><a href="http://f1097116.xsph.ru/lab2/Memento.php">Memento</a></li>
+        <li><a href="http://f1097116.xsph.ru/lab2/Observer.php">Observer</a></li>
+        <li><a href="http://f1097116.xsph.ru/lab2/State.php">State</a></li>
+        <li><a href="http://f1097116.xsph.ru/lab2/Strategy.php">Strategy</a></li>
+        <li><a href="http://f1097116.xsph.ru/lab2/TemplateMetod.php">Template Method</a></li>
+        <li><a href="http://f1097116.xsph.ru/lab2/Visitor.php">Visitor</a></li>
 </nav>
 
 </body>
