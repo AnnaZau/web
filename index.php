82c23938d7f77c6637e01517a336d4d84028e40e
AnnaZau <126783930+AnnaZau@users.noreply.github.com>
2025-05-03 13:59:41 +0300

    Update index.php

--- index.php
+++ index.php
@@ -33,7 +33,19 @@
         <li><a href="http://f1097116.xsph.ru/lab2/Strategy.php">Strategy</a></li>
         <li><a href="http://f1097116.xsph.ru/lab2/TemplateMetod.php">Template Method</a></li>
         <li><a href="http://f1097116.xsph.ru/lab2/Visitor.php">Visitor</a></li>
+        <lu><a> Лабораторная работа № 3</a></lu>
+        <li> <a href="http://f1097116.xsph.ru/lab3/patterns/singleton/settings_use.php">Singleton</a></li>
+        <li> <a href="http://f1097116.xsph.ru/lab3/patterns/factory_use.php">Factory Method</a></li>
+        <li> <a href="https://img.plantuml.biz/plantuml/png/hLJBQiCm4BmR_0zxo26XoG-4bWRw0w7Gir5jDGXaoQnMVT3ctol9ZYM_GWFLoTuPpSokP1yCCIHRopm3jzYh8MGbGIcPCV2iBN64xnRqwwvXQFZQiOGwmSgYtEoX5f-yIZZ8LSLnxP1tBJhWd6UHqQeVsOcdsc4l2P9y3wt122kr8gve0IqAXpl4bjJd2sfRkBns0flHpfXtB7oEZ-FqJrfANfBGQj93aYGTmH_y4jWiv1ddU3734meIl6P2BiY_2JHKA5QxCJ_ECfpkTSBSg8Db77NZidrehBON2LX5OtW0PQLCiDwoWsuwehcBPIOpQNpLTSGGsTULXbg2xy2h_4CSVqBJEDPNRuWJ-DCumQ_xJkz_ThlWDgp4cpgi-p7bMNAF-wgPbaCJtklWMW__Yry0">Диаграмма классов</a></li>
+        <li> <a href="http://f1097116.xsph.ru/lab3/patterns/mvc/mvc_use.php">MVC</a></li>
+        <li> <a href="https://www.plantuml.com/plantuml/svg/RL513e8m4Bpt5HjE4k437Wo9yQW74xsQJMa8iHIpBHgZ_DsbWb3qs9cTcTrfPXsIQoeDrKqASKLvmK91nAy8dW90QhGMZbRHfZQEQgqLUG8WXOPX2oj08dnuR7a-ozfOHurqYnW-61JATGuCZBEj6As_XWOhnJEiBqtHOvpvWwaAIns2GNBWauGMoKmTZqlkMUQNdFfsn84D_XBRpYfaXiDsI9UylfjJgUxzWKiWPN8k8aU7GnSpSIk4cDMTfg-rx-alCSrXRSO9_9_Y3G00.svg">Диаграмма классов</a></li>
+        <lu><a> Лабораторная работа №4</a></lu>
+        <li><a href="http://f1097116.xsph.ru/lab4/news/news.php">Задание 1</a></li>
+        <li><a href=https://img.plantuml.biz/plantuml/svg/LL7BJiD03Bn7oZzOvTA891Tk5Q0IfGSaMWbLdAFDnaGhfRl5Te08-7VoR9gRFPwHP-mjYoAfJnMyCTAnOS6Jx_cUiOBqhZJ2ym6_E4tWn_S0Re7L9tR8MenK689MWgMZ9eJ2i2Pp5kDi2EngqXXiSkSgL7RGbYYzDDY08bBDW1POeU0qk0Z-EXkwKiomTA2TPO4qoGvF-nqymCgsuxZ8Lzquoefy0yVN5oDur_Epda-rbdKm00Kk-akYfTlPxEnrOVMAVDEIqwJ5hXOl4epu506_KO_RuQD6acRiaBKwur78s7A4MhtiY1pjUO9qvHYxkjqmlUV-DuwdG_dUM4JHutmBtzkYBTfN_mC0>Диаграмма классов</a></li>
+    </ol>
 </nav>
 
 </body>
+</html>
+
 </html>
