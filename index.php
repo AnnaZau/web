a21cc592640e40975ae49938e0df1ecec7735481
AnnaZau <126783930+AnnaZau@users.noreply.github.com>
2025-05-03 14:05:37 +0300

    Update index.php

--- index.php
+++ index.php
@@ -40,7 +40,7 @@
         <li> <a href="http://f1097116.xsph.ru/lab3/patterns/mvc/mvc_use.php">MVC</a></li>
         <li> <a href="https://www.plantuml.com/plantuml/svg/RL513e8m4Bpt5HjE4k437Wo9yQW74xsQJMa8iHIpBHgZ_DsbWb3qs9cTcTrfPXsIQoeDrKqASKLvmK91nAy8dW90QhGMZbRHfZQEQgqLUG8WXOPX2oj08dnuR7a-ozfOHurqYnW-61JATGuCZBEj6As_XWOhnJEiBqtHOvpvWwaAIns2GNBWauGMoKmTZqlkMUQNdFfsn84D_XBRpYfaXiDsI9UylfjJgUxzWKiWPN8k8aU7GnSpSIk4cDMTfg-rx-alCSrXRSO9_9_Y3G00.svg">Диаграмма классов</a></li>
         <lu><a> Лабораторная работа №4</a></lu>
-        <li><a href="http://f1097116.xsph.ru/lab4/news/news.php">Задание 1</a></li>
+        <li><a href="http://f1097116.xsph.ru/lab4/news.php">Задание 1</a></li>
         <li><a href=https://img.plantuml.biz/plantuml/svg/LL7BJiD03Bn7oZzOvTA891Tk5Q0IfGSaMWbLdAFDnaGhfRl5Te08-7VoR9gRFPwHP-mjYoAfJnMyCTAnOS6Jx_cUiOBqhZJ2ym6_E4tWn_S0Re7L9tR8MenK689MWgMZ9eJ2i2Pp5kDi2EngqXXiSkSgL7RGbYYzDDY08bBDW1POeU0qk0Z-EXkwKiomTA2TPO4qoGvF-nqymCgsuxZ8Lzquoefy0yVN5oDur_Epda-rbdKm00Kk-akYfTlPxEnrOVMAVDEIqwJ5hXOl4epu506_KO_RuQD6acRiaBKwur78s7A4MhtiY1pjUO9qvHYxkjqmlUV-DuwdG_dUM4JHutmBtzkYBTfN_mC0>Диаграмма классов</a></li>
     </ol>
 </nav>
